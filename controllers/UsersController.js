f2efe34700fc17bfba6ad8220bfe5afe12964c7a
AlphonseMehounme <alphonsemehounme9@gmail.com>
2024-10-14 17:59:51 +0100

    return error when email not provided

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -3,21 +3,22 @@ const dbClient = require('../utils/db');
 
 const postNew = (async (req, res) => {
   if (!req.body.email) {
-    return res.status(400).send('Missing email');
-  } else if (!req.body.password) {
-    return res.status(400).send('Missing password');
-  } else {
-    const { email, password } = req.body;
-    const userfound = await dbClient.userFound(email);
-    if (userfound) {
-      return res.status(400).send('Already exist');
-    } else {
-      const hashedPassword = crypto.createHash('sha1').update(password).digest('hex');
-      const user = await dbClient.addUser(email, hashedPassword);
-      const userInfo = { id: user._id, email: user.email };
-      return res.status(201).send(userInfo);
-    }
+    //const er = new Error('Missing email');
+    return res.status(400).send({ error: 'Missing email' });
+    
   }
+  if (!req.body.password) {
+    return res.status(400).send('Missing password');
+  }
+  const { email, password } = req.body;
+  const userfound = await dbClient.userFound(email);
+  if (userfound) {
+    return res.status(400).send('Already exist');
+  } 
+  const hashedPassword = crypto.createHash('sha1').update(password).digest('hex');
+  const user = await dbClient.addUser(email, hashedPassword);
+  const userInfo = { id: user._id, email: user.email };
+  return res.status(201).send(userInfo);
 });
 
 const getMe = (req, res) => {
