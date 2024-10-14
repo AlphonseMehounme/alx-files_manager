6fa013c4c1b492c572d96802f6ccb39ada20d569
AlphonseMehounme <alphonsemehounme9@gmail.com>
2024-10-14 21:19:55 +0100

    change the return to error objects

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -4,16 +4,15 @@ const dbClient = require('../utils/db');
 const postNew = (async (req, res) => {
   if (!req.body.email) {
     //const er = new Error('Missing email');
-    return res.status(400).send({ error: 'Missing email' });
-    
+    return res.status(400).send({ error: 'Missing email' });  
   }
   if (!req.body.password) {
-    return res.status(400).send('Missing password');
+    return res.status(400).send({ error: 'Missing password' });
   }
   const { email, password } = req.body;
   const userfound = await dbClient.userFound(email);
   if (userfound) {
-    return res.status(400).send('Already exist');
+    return res.status(400).send({ error: 'Already exist' });
   } 
   const hashedPassword = crypto.createHash('sha1').update(password).digest('hex');
   const user = await dbClient.addUser(email, hashedPassword);
