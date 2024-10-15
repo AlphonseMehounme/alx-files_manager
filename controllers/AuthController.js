cdbf5af15b7724ad0f8d4aeb2897b8ada212fec5
Alphonse Mehounme <52801872+AlphonseMehounme@users.noreply.github.com>
2024-10-15 08:48:53 +0100

    Update AuthController.js

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -3,4 +3,8 @@ const getConnect = (req, res) => {
     res.status(200).send('Get connect');
 }
 
+const getDisconnect = (req, res) => {
+    res.status(200).send('Get disconnect');
+}
+
 module.exports = { getConnect };
