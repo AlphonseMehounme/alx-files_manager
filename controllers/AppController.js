0346c159d2d4a3c0ffca3b5a20c9fc503ed678f6
AlphonseMehounme <alphonsemehounme9@gmail.com>
2024-10-16 05:45:20 -0700

    change router format

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,7 +1,7 @@
 const redisClient = require('../utils/redis');
 const dbClient = require('../utils/db');
 
-const getStatus = ((req, res) => {
+/*const getStatus = ((req, res) => {
   if (redisClient.isAlive() && dbClient.isAlive()) {
     res.status(200).send({ redis: redisClient.isAlive(), db: dbClient.isAlive() });
   }
@@ -13,4 +13,14 @@ const getStats = (async (req, res) => {
   res.status(200).send({ users: nbUsers, files: nbFiles });
 });
 
-module.exports = { getStatus, getStats };
+module.exports = { getStatus, getStats };*/
+
+class AppController {
+  static getStatus(req, res) {
+    if (redisClient.isAlive() && dbClient.isAlive()) {
+      res.status(200).send({ redis: redisClient.isAlive(), db: dbClient.isAlive() });
+    }
+  }
+}
+
+export default AppController;
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
 const express = require('express');
 import UsersController from '../controllers/UsersController';
 import AuthController from '../controllers/AuthController';
+import AppController from '../controllers/AppController';
 const router = express.Router();
 
-const {
-  getStatus,
-  getStats,
-} = require('../controllers/AppController');
+//const {
+//  getStatus,
+//  getStats,
+//} = {0, 0}; //require('../controllers/AppController');
 
 /*const {
   postNew,
@@ -18,9 +19,13 @@ const {
   getDisconnect,
 } = require('../controllers/AuthController');*/
 
-router.get('/status', getStatus);
+router.get('/status', (req, res) => {
+  AppController.getStatus(req, res);
+});
 
-router.get('/stats', getStats);
+router.get('/stats', (req, res) => {
+  AppController.getStats(req, res);
+});
 
 router.post('/users', (req, res) => {
   UsersController.postNew(req, res);
