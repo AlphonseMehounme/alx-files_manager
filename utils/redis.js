183499e112dcf2ee79f95c6a493052a3fd1fe72f
AlphonseMehounme <alphonsemehounme9@gmail.com>
2024-10-16 00:31:06 -0700

    fix isalive function

--- utils/redis.js
+++ utils/redis.js
@@ -3,23 +3,15 @@ import { promisify } from 'util';
 
 class RedisClient {
   constructor() {
+    this.connexion = false;
     this.client = redis.createClient();
     this.newGet = promisify(this.client.get).bind(this.client);
-    this.connexion = false;
     this.client.on('error', (err) => console.log('Connexion error', err.message));
     this.client.on('connect', () => {
-      this.client.connexion = true;
+      // this.connexion = true;
     });
+    this.connexion = true;
   }
-
-  /*async connectToRedis() {
-    try {
-      await this.client.connect();
-      this.connexion = true;
-    } catch (err) {
-      console.log('First connexion attemps to redis failed', err);
-    }
-  }*/
 
   isAlive() {
     return this.connexion;
